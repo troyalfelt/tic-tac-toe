2ef996d094f219fb6ae0f17037db752b660f526c
Troy Alfelt <troyalfelt@gmail.com>
2020-11-11 12:38:03 -0500

    used prettier to format code

--- assets/main.js
+++ assets/main.js
@@ -3,10 +3,10 @@ let gridNodes = grid.childNodes;
 
 let gridBox = document.querySelectorAll(".gridBox");
 
-    for (let index of gridBox) {
-        paragraph = document.createElement("p");
-        index.appendChild(paragraph);
-    };
+for (let index of gridBox) {
+  paragraph = document.createElement("p");
+  index.appendChild(paragraph);
+}
 
 let topLeft = document.querySelector("#topLeft");
 let topMiddle = document.querySelector("#topMiddle");
@@ -20,95 +20,135 @@ let bottomRight = document.querySelector("#bottomRight");
 
 let player = "X";
 
-const checkWin = function() {
-   if (topLeft.childNodes[0].textContent.length > 0) {
-     if (topLeft.childNodes[0].textContent == middleLeft.childNodes[0].textContent
-     && topLeft.childNodes[0].textContent == bottomLeft.childNodes[0].textContent) {
-     topLeft.style.backgroundColor = 'green';
-     middleLeft.style.backgroundColor = 'green';
-     bottomLeft.style.backgroundColor = 'green';
-     for (let index of gridNodes) {
-      index.removeEventListener("click", clickBox);
-        };
-      } else if (topLeft.childNodes[0].textContent == topMiddle.childNodes[0].textContent
-        && topLeft.childNodes[0].textContent == topRight.childNodes[0].textContent) {
-        topLeft.style.backgroundColor = 'green';
-        topMiddle.style.backgroundColor = 'green';
-        topRight.style.backgroundColor = 'green';
-        for (let index of gridNodes) {
-         index.removeEventListener("click", clickBox);
-        };
-      } else if (topLeft.childNodes[0].textContent == middleMiddle.childNodes[0].textContent
-        && topLeft.childNodes[0].textContent == bottomRight.childNodes[0].textContent) {
-          topLeft.style.backgroundColor = 'green';
-          middleMiddle.style.backgroundColor = 'green';
-          bottomRight.style.backgroundColor = 'green';
-          for (let index of gridNodes) {
-           index.removeEventListener("click", clickBox);
-          };
-        };
-      };
-     if (middleMiddle.childNodes[0].textContent.length > 0) {
-      if (middleMiddle.childNodes[0].textContent == topMiddle.childNodes[0].textContent
-          && middleMiddle.childNodes[0].textContent == bottomMiddle.childNodes[0].textContent) {
-            middleMiddle.style.backgroundColor = 'green';
-            topMiddle.style.backgroundColor = 'green';
-            bottomMiddle.style.backgroundColor = 'green';
-            for (let index of gridNodes) {
-             index.removeEventListener("click", clickBox);
-            };
-          } else if (middleMiddle.childNodes[0].textContent == middleLeft.childNodes[0].textContent
-            && middleMiddle.childNodes[0].textContent == middleRight.childNodes[0].textContent) {
-              middleMiddle.style.backgroundColor = 'green';
-              middleLeft.style.backgroundColor = 'green';
-              middleRight.style.backgroundColor = 'green';
-              for (let index of gridNodes) {
-               index.removeEventListener("click", clickBox);
-              };
-            } else if (middleMiddle.childNodes[0].textContent == bottomLeft.childNodes[0].textContent
-              && middleMiddle.childNodes[0].textContent == topRight.childNodes[0].textContent) {
-                middleMiddle.style.backgroundColor = 'green';
-                bottomLeft.style.backgroundColor = 'green';
-                topRight.style.backgroundColor = 'green';
-                for (let index of gridNodes) {
-                 index.removeEventListener("click", clickBox);
-                };
-              };
-            };
-      if (bottomRight.childNodes[0].textContent.length > 0) {
-        if (bottomRight.childNodes[0].textContent == bottomMiddle.childNodes[0].textContent
-        && bottomRight.childNodes[0].textContent == bottomLeft.childNodes[0].textContent) {
-          bottomRight.style.backgroundColor = 'green';
-          bottomLeft.style.backgroundColor = 'green';
-          bottomMiddle.style.backgroundColor = 'green';
-          for (let index of gridNodes) {
-           index.removeEventListener("click", clickBox);
-          };
-        } else if (bottomRight.childNodes[0].textContent == middleRight.childNodes[0].textContent
-          && bottomRight.childNodes[0].textContent == topRight.childNodes[0].textContent) {
-            bottomRight.style.backgroundColor = 'green';
-            middleRight.style.backgroundColor = 'green';
-            topRight.style.backgroundColor = 'green';
-            for (let index of gridNodes) {
-             index.removeEventListener("click", clickBox);
-            };
-          };
-        };
-      };
+const checkWin = function () {
+  if (topLeft.childNodes[0].textContent.length > 0) {
+    if (
+      topLeft.childNodes[0].textContent ==
+        middleLeft.childNodes[0].textContent &&
+      topLeft.childNodes[0].textContent == bottomLeft.childNodes[0].textContent
+    ) {
+      topLeft.style.backgroundColor = "green";
+      middleLeft.style.backgroundColor = "green";
+      bottomLeft.style.backgroundColor = "green";
+      for (let index of gridNodes) {
+        index.removeEventListener("click", clickBox);
+      }
+    } else if (
+      topLeft.childNodes[0].textContent ==
+        topMiddle.childNodes[0].textContent &&
+      topLeft.childNodes[0].textContent == topRight.childNodes[0].textContent
+    ) {
+      topLeft.style.backgroundColor = "green";
+      topMiddle.style.backgroundColor = "green";
+      topRight.style.backgroundColor = "green";
+      for (let index of gridNodes) {
+        index.removeEventListener("click", clickBox);
+      }
+    } else if (
+      topLeft.childNodes[0].textContent ==
+        middleMiddle.childNodes[0].textContent &&
+      topLeft.childNodes[0].textContent == bottomRight.childNodes[0].textContent
+    ) {
+      topLeft.style.backgroundColor = "green";
+      middleMiddle.style.backgroundColor = "green";
+      bottomRight.style.backgroundColor = "green";
+      for (let index of gridNodes) {
+        index.removeEventListener("click", clickBox);
+      }
+    }
+  }
+  if (middleMiddle.childNodes[0].textContent.length > 0) {
+    if (
+      middleMiddle.childNodes[0].textContent ==
+        topMiddle.childNodes[0].textContent &&
+      middleMiddle.childNodes[0].textContent ==
+        bottomMiddle.childNodes[0].textContent
+    ) {
+      middleMiddle.style.backgroundColor = "green";
+      topMiddle.style.backgroundColor = "green";
+      bottomMiddle.style.backgroundColor = "green";
+      for (let index of gridNodes) {
+        index.removeEventListener("click", clickBox);
+      }
+    } else if (
+      middleMiddle.childNodes[0].textContent ==
+        middleLeft.childNodes[0].textContent &&
+      middleMiddle.childNodes[0].textContent ==
+        middleRight.childNodes[0].textContent
+    ) {
+      middleMiddle.style.backgroundColor = "green";
+      middleLeft.style.backgroundColor = "green";
+      middleRight.style.backgroundColor = "green";
+      for (let index of gridNodes) {
+        index.removeEventListener("click", clickBox);
+      }
+    } else if (
+      middleMiddle.childNodes[0].textContent ==
+        bottomLeft.childNodes[0].textContent &&
+      middleMiddle.childNodes[0].textContent ==
+        topRight.childNodes[0].textContent
+    ) {
+      middleMiddle.style.backgroundColor = "green";
+      bottomLeft.style.backgroundColor = "green";
+      topRight.style.backgroundColor = "green";
+      for (let index of gridNodes) {
+        index.removeEventListener("click", clickBox);
+      }
+    }
+  }
+  if (bottomRight.childNodes[0].textContent.length > 0) {
+    if (
+      bottomRight.childNodes[0].textContent ==
+        bottomMiddle.childNodes[0].textContent &&
+      bottomRight.childNodes[0].textContent ==
+        bottomLeft.childNodes[0].textContent
+    ) {
+      bottomRight.style.backgroundColor = "green";
+      bottomLeft.style.backgroundColor = "green";
+      bottomMiddle.style.backgroundColor = "green";
+      for (let index of gridNodes) {
+        index.removeEventListener("click", clickBox);
+      }
+    } else if (
+      bottomRight.childNodes[0].textContent ==
+        middleRight.childNodes[0].textContent &&
+      bottomRight.childNodes[0].textContent ==
+        topRight.childNodes[0].textContent
+    ) {
+      bottomRight.style.backgroundColor = "green";
+      middleRight.style.backgroundColor = "green";
+      topRight.style.backgroundColor = "green";
+      for (let index of gridNodes) {
+        index.removeEventListener("click", clickBox);
+      }
+    }
+  }
+  let counter = 0;
+  for (let index of gridNodes) {
+    if (index.childNodes[0].length > 0) {
+      counter += 1;
+    }
+  }
+  if (counter == 9) {
+    for (let index of gridNodes) {
+      index.style.backgroundColor = "yellow";
+    }
+  }
+};
 
-const clickBox = function(evt) {
-  let paragraphSymbol = evt.target.childNodes[0]
+const clickBox = function (evt) {
+  let paragraphSymbol = evt.target.childNodes[0];
   paragraphSymbol.textContent = player;
   if (player == "X") {
     player = "O";
   } else {
-    player = "X"
-  };
+    player = "X";
+  }
 
   evt.target.removeEventListener("click", clickBox);
   return checkWin();
 };
 
- for (let index of gridNodes) {
-     index.addEventListener("click", clickBox)
- };
+for (let index of gridNodes) {
+  index.addEventListener("click", clickBox);
+}
